9f00fd8eb072bd290cfa3f0bff2bc2d448e5707b
Julio Cano <julioalbertocano@gmail.com>
2022-01-11 18:14:38 -0500

    Initial Commit

---
+++ src/main/java/com/trabajodegrado/freshfruitventas/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.trabajodegrado.freshfruitventas;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FreshfruitventasApplication.class);
+	}
+
+}
